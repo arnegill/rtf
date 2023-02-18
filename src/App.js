7987bd54b676735e8bcc8811d907175b406f447c
arnegill <arnegill@hotmail.com>
2023-02-18 09:49:43 +0000

    added 3d button to link to my github

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
+import React, { useRef, useState, Suspense } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import './style.css'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from '@react-three/drei'
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef()
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false)
-  const [active, setActive] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += delta))
-  // Return view, these are regular three.js elements expressed in JSX
+const Model = () => {
+  const ref = useRef()
+  const gltf = useLoader(GLTFLoader, "./gitlogo.glb");
+  useFrame((state, delta) => (ref.current.rotation.y += delta))
+  const [hovered, hover] = useState(false)
+
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
-function App() {
+      <primitive
+        ref={ref}
+        object={gltf.scene}
+        scale={hovered ? 0.4: 0.3}  
+        rotation={[0,0,-1.5708]}
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}
+        onClick={() => window.open('https://github.com/arnegill', '_blank', 'noreferrer')}
+      />
+  );
+};
+
+export default function App() {
   return (
     <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      <OrbitControls />
-    </Canvas>
+        <Suspense fallback={null}>
+          <ambientLight />
+          <Model/>
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
   );
 }
 
-export default App;
