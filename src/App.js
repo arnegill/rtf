a98b9d2a146c5715af605ffd817374b19da34bc8
arnegill <arnegill@hotmail.com>
2023-02-23 07:06:45 +0000

    finalized

--- src/App.js
+++ src/App.js
@@ -1,50 +1,120 @@
-import React, { Suspense, useRef, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import './style.css'
-import {OrbitControls, RandomizedLight, AccumulativeShadows, Lightformer,Environment} from '@react-three/drei'
-import Keyboardv2 from './Keyboardv2'
-import Screen from './Screen'
-import Cord from './Cord'
-import Screenv2 from './Screenv2'
-import Screenv4 from './Screenv4'
-import Keyboardv3 from './Keyboardv3'
+import React, { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import "./style.css";
+import {
+  OrbitControls,
+  RandomizedLight,
+  AccumulativeShadows,
+  Lightformer,
+  Environment,
+} from "@react-three/drei";
+import Cord from "./Cord";
+import Screenv4 from "./Screenv4";
+import Keyboardv3 from "./Keyboardv3";
 
 function App() {
-  const [selectedVideo, changeSelectedVideo] = useState('/mordor.mp4')
-  function ChangeToMine(vid){
-    changeSelectedVideo(vid)
+  const [selectedVideo, changeSelectedVideo] = useState("/mordor.mp4");
+  function ChangeToMine(vid) {
+    changeSelectedVideo(vid);
   }
   return (
     <Canvas shadows camera={{ position: [15, 15, 30], fov: 15 }}>
-      <color attach="background" args={['#f0f0f0']} />
+      <color attach="background" args={["#f0f0f0"]} />
       <Suspense>
-        <group position={[5,0,0]}>
-          <Screenv4 position={[2, 2.2, 2]} rotation={[Math.PI/2, 0, Math.PI/2]} scale={1} vid={selectedVideo}/>
-          <Keyboardv3 change={ChangeToMine} rotation = {[0,-0.4,0]} position={[-4, 0.1, 5]} mine={()=>ChangeToMine()}/>
-          <Cord scale={1.5} position = {[1, 0.1, 4]} rotation={[0,0.5,0]}/>
+        <group position={[5, 0, 0]}>
+          <Screenv4
+            position={[2, 2.2, 2]}
+            rotation={[Math.PI / 2, 0, Math.PI / 2]}
+            scale={1}
+            vid={selectedVideo}
+          />
+          <Keyboardv3
+            change={ChangeToMine}
+            rotation={[0, -0.4, 0]}
+            position={[-4, 0.1, 5]}
+            mine={() => ChangeToMine()}
+          />
+          <Cord scale={1.5} position={[1, 0.1, 4]} rotation={[0, 0.5, 0]} />
         </group>
-        <AccumulativeShadows frames={100} alphaTest={0.85} opacity={0.8} color="red" scale={100} position={[0, -0.005, 0]}>
-        <RandomizedLight amount={8} radius={6} ambient={0.5} intensity={1} position={[-1.5, 2.5, -2.5]} bias={0.001} />
+        <AccumulativeShadows
+          frames={100}
+          alphaTest={0.85}
+          opacity={0.8}
+          color="red"
+          scale={100}
+          position={[0, -0.005, 0]}
+        >
+          <RandomizedLight
+            amount={8}
+            radius={6}
+            ambient={0.5}
+            intensity={1}
+            position={[-1.5, 2.5, -2.5]}
+            bias={0.001}
+          />
         </AccumulativeShadows>
       </Suspense>
-                <Environment  preset="city" resolution={256} background blur={0.8}>
-                <Lightformer intensity={1} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
-                <Lightformer intensity={1} rotation-x={Math.PI / 2} position={[0, 5, 9]} scale={[10, 10, 1]} />
-                <group rotation={[Math.PI / 2, 1, 0]}>
-                  {[2, -2, 2, -4, 2, -5, 2, -9].map((x, i) => (
-                    <Lightformer key={i} intensity={1} rotation={[Math.PI / 4, 0, 0]} position={[x, 4, i * 4]} scale={[4, 1, 1]} />
-                  ))}
-                  <Lightformer intensity={0.2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[50, 2, 1]} />
-                  <Lightformer intensity={0.2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[50, 2, 1]} />
-                  <Lightformer intensity={0.2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[50, 2, 1]} />
-                </group>
-                <group>
-                  <Lightformer intensity={5} form="ring" color="blue" rotation-y={Math.PI / 2} position={[-5, 2, -1]} scale={[10, 10, 1]} />
-                </group>
-              </Environment>
-      <spotLight position={[-30, 50, 10]} angle={0.15} penumbra={1} />
-      <ambientLight intensity={0.1}/>
-      <OrbitControls />
+      <Environment preset="city" resolution={256} background blur={0.8}>
+        <Lightformer
+          intensity={1}
+          rotation-x={Math.PI / 2}
+          position={[0, 5, -9]}
+          scale={[10, 10, 1]}
+        />
+        <Lightformer
+          intensity={1}
+          rotation-x={Math.PI / 2}
+          position={[0, 5, 9]}
+          scale={[10, 10, 1]}
+        />
+        <group rotation={[Math.PI / 2, 1, 0]}>
+          
+          <Lightformer
+            intensity={0.2}
+            rotation-y={Math.PI / 2}
+            position={[-5, 1, -1]}
+            scale={[50, 2, 1]}
+          />
+          <Lightformer
+            intensity={0.2}
+            rotation-y={Math.PI / 2}
+            position={[-5, -1, -1]}
+            scale={[50, 2, 1]}
+          />
+          <Lightformer
+            intensity={0.2}
+            rotation-y={-Math.PI / 2}
+            position={[10, 1, 0]}
+            scale={[50, 2, 1]}
+          />
+        </group>
+        <group>
+          <Lightformer
+            intensity={5}
+            form="ring"
+            color="red"
+            rotation-y={Math.PI / 2}
+            position={[-5, 2, -1]}
+            scale={[10, 10, 1]}
+          />
+          <Lightformer
+            intensity={5}
+            form="ring"
+            color="pink"
+            rotation-y={Math.PI / 2}
+            position={[5, -2, 1]}
+            scale={[10, 10, 1]}
+          />
+        </group>
+      </Environment>
+      <OrbitControls
+        minAzimuthAngle={-Math.PI / 4}
+        maxAzimuthAngle={Math.PI / 4}
+        minPolarAngle={0}
+        maxPolarAngle={Math.PI/2-0.1}
+        minDistance={20}
+        maxDistance={100}
+      />
     </Canvas>
   );
 }
